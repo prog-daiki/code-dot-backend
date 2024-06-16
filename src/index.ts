a306261b4da8d76b9c28d7a2853517bbd7fbda5b
prog-daiki <t.daiki0913@icloud.com>
2024-06-16 18:06:44 +0900

    不要な記述の削除

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 import { Hono } from "hono";
-import { HTTPException } from "hono/http-exception";
 import CourseController from "./controller/CourseController";
 import { clerkMiddleware } from "@hono/clerk-auth";
 
 const app = new Hono().basePath("/api");
 app.use("*", clerkMiddleware());
-
-app.onError((err, c) => {
-  if (err instanceof HTTPException) {
-    return err.getResponse();
-  }
-
-  return c.json({ error: "Internal Server Error" }, 500);
-});
 
 const routes = app.route("/courses", CourseController);
 
