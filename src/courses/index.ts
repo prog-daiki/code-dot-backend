a974c088a40f0ba593bf0048b1c7aa4b71efd71b
prog-daiki <t.daiki0913@icloud.com>
2024-06-23 20:35:04 +0900

    スキーマ名の修正

--- src/courses/index.ts
+++ src/courses/index.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { Hono } from "hono";
 import { getDbConnection } from "../../db/drizzle";
-import { courses } from "../../db/schema";
+import { course } from "../../db/schema";
 import { Env } from "..";
 import { and, eq } from "drizzle-orm";
 import { Messages } from "../sharedInfo/message";
@@ -18,42 +18,42 @@ Course.get("/", clerkMiddleware(), async (c) => {
   }
 
   const db = getDbConnection(c.env.DATABASE_URL);
-  const result = await db
+  const data = await db
     .select()
-    .from(courses)
-    .where(and(eq(courses.deleteFlag, false), eq(courses.publishFlag, true)));
+    .from(course)
+    .where(and(eq(course.deleteFlag, false), eq(course.publishFlag, true)));
 
-  return c.json({ result });
+  return c.json({ data });
 });
 
 /**
  * 講座取得API
  */
-Course.get("/:id", clerkMiddleware(), async (c) => {
+Course.get("/:course_id", clerkMiddleware(), async (c) => {
   const auth = getAuth(c);
   if (!auth?.userId) {
     return c.json({ error: Messages.ERR_UNAUTHORIZED }, 401);
   }
 
-  const id = c.req.param("id");
+  const courseId = c.req.param("course_id");
 
   const db = getDbConnection(c.env.DATABASE_URL);
-  const result = await db
+  const data = await db
     .select()
-    .from(courses)
+    .from(course)
     .where(
       and(
-        eq(courses.id, id),
-        eq(courses.deleteFlag, false),
-        eq(courses.publishFlag, true)
+        eq(course.id, courseId),
+        eq(course.deleteFlag, false),
+        eq(course.publishFlag, true)
       )
     );
 
-  if (!result) {
+  if (!data) {
     return c.json({ error: Messages.ERR_COURSE_NOT_FOUND }, 404);
   }
 
-  return c.json({ result });
+  return c.json({ data });
 });
 
 export default Course;
