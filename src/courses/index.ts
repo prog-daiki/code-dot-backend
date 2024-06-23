65f68b3a7bb1a5a5935f70082daed50a24248fbb
prog-daiki <t.daiki0913@icloud.com>
2024-06-23 18:09:41 +0900

    メッセージの修正

--- src/courses/index.ts
+++ src/courses/index.ts
@@ -4,10 +4,7 @@ import { getDbConnection } from "../../db/drizzle";
 import { courses } from "../../db/schema";
 import { Env } from "..";
 import { and, eq } from "drizzle-orm";
-import {
-  MSG_ERR_COURSE_NOT_FOUND,
-  MSG_ERR_UNAUTHORIZED,
-} from "../sharedInfo/message";
+import { Messages } from "../sharedInfo/message";
 
 const CourseController = new Hono<{ Bindings: Env }>();
 
@@ -18,7 +15,7 @@ CourseController.get("/", clerkMiddleware(), async (c) => {
   // ユーザーの認証チェック
   const auth = getAuth(c);
   if (!auth?.userId) {
-    return c.json({ error: MSG_ERR_UNAUTHORIZED }, 401);
+    return c.json({ error: Messages.ERR_UNAUTHORIZED }, 401);
   }
 
   // 講座の一覧を取得する
@@ -38,7 +35,7 @@ CourseController.get("/:id", clerkMiddleware(), async (c) => {
   // ユーザーの認証チェック
   const auth = getAuth(c);
   if (!auth?.userId) {
-    return c.json({ error: MSG_ERR_UNAUTHORIZED }, 401);
+    return c.json({ error: Messages.ERR_UNAUTHORIZED }, 401);
   }
 
   // 講座のIDを取得する
@@ -59,7 +56,7 @@ CourseController.get("/:id", clerkMiddleware(), async (c) => {
 
   // 講座が見つからない場合は404を返す
   if (!result) {
-    return c.json({ error: MSG_ERR_COURSE_NOT_FOUND }, 404);
+    return c.json({ error: Messages.ERR_COURSE_NOT_FOUND }, 404);
   }
 
   return c.json({ result });
--- src/sharedInfo/message.ts
+++ src/sharedInfo/message.ts
@@ -1,2 +1,4 @@
-export const MSG_ERR_UNAUTHORIZED = "認証されていません";
-export const MSG_ERR_COURSE_NOT_FOUND = "存在しない講座です";
+export const Messages = {
+  ERR_UNAUTHORIZED: "認証されていません",
+  ERR_COURSE_NOT_FOUND: "存在しない講座です",
+};
