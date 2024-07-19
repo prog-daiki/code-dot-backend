bad01f5a163cf2b570c33b1441a661ead95ffee5
prog-daiki <t.daiki0913@icloud.com>
2024-07-20 08:45:11 +0900

    リファクタリング：カテゴリー一覧取得API、カテゴリー登録API

--- src/categories/index.ts
+++ src/categories/index.ts
@@ -1,12 +1,10 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { category, insertCategorySchema } from "../../db/schema";
+import { insertCategorySchema } from "../../db/schema";
 import { getAuth } from "@hono/clerk-auth";
-import { Messages } from "../sharedInfo/message";
+import { Length, Messages, Property } from "../sharedInfo/message";
 import { Env } from "..";
 import { getDbConnection } from "../../db/drizzle";
-import { getJstDate } from "../sharedInfo/date";
-import { createId } from "@paralleldrive/cuid2";
 import { CategoryLogic } from "./logic";
 
 const Category = new Hono<{ Bindings: Env }>();
@@ -18,7 +16,7 @@ Category.get("/", async (c) => {
   // 認証チェック
   const auth = getAuth(c);
   if (!auth?.userId) {
-    return c.json({ error: Messages.ERR_UNAUTHORIZED }, 401);
+    return c.json({ error: Messages.MSG_ERR_001 }, 401);
   }
 
   // データベース接続
@@ -46,35 +44,40 @@ Category.post(
     // 認証チェック
     const auth = getAuth(c);
     if (!auth?.userId) {
-      return c.json({ error: Messages.ERR_UNAUTHORIZED }, 401);
+      return c.json({ error: Messages.MSG_ERR_001 }, 401);
     }
     if (auth.userId !== c.env.ADMIN_USER_ID) {
-      return c.json({ error: Messages.MSG_ERR_ADMIN_UNAUTHORIZED }, 401);
+      return c.json({ error: Messages.MSG_ERR_002 }, 401);
     }
 
     // バリデーションチェック
     const values = c.req.valid("json");
     if (!values.name) {
-      return c.json({ error: Messages.MSG_ERR_CATEGORY_REQUIRED }, 400);
+      return c.json(
+        { error: Messages.MSG_ERR_004(Property.CATEGORY_NAME) },
+        400
+      );
     }
-    if (values.name.length >= 100) {
-      return c.json({ error: Messages.MSG_ERR_CATEGORY_LIMIT }, 400);
+    if (values.name.length > 100) {
+      return c.json(
+        {
+          error: Messages.MSG_ERR_005(
+            Property.CATEGORY_NAME,
+            Length.CATEGORY_NAME
+          ),
+        },
+        400
+      );
     }
 
     // データベース接続
     const db = getDbConnection(c.env.DATABASE_URL);
+    const categoryLogic = new CategoryLogic(db);
 
     // データベースへの登録
-    const currentJstDate = getJstDate();
-    const [data] = await db
-      .insert(category)
-      .values({
-        id: createId(),
-        name: values.name,
-      })
-      .returning();
+    const category = await categoryLogic.registerCategory(values);
 
-    return c.json({ data });
+    return c.json(category);
   }
 );
 
--- src/categories/logic/index.ts
+++ src/categories/logic/index.ts
@@ -2,6 +2,7 @@ import { PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import * as schema from "../../../db/schema";
 import { asc, eq } from "drizzle-orm";
 import { category } from "../../../db/schema";
+import { createId } from "@paralleldrive/cuid2";
 /**
  * カテゴリーのロジックを管理するクラス
  */
@@ -31,5 +32,21 @@ export class CategoryLogic {
       .from(category)
       .where(eq(category.id, categoryId));
     return !!existCategory;
+  }
+
+  /**
+   * カテゴリーを登録する
+   * @param values
+   * @returns
+   */
+  async registerCategory(values: Pick<typeof category.$inferInsert, "name">) {
+    const [data] = await this.db
+      .insert(category)
+      .values({
+        id: createId(),
+        ...values,
+      })
+      .returning();
+    return data;
   }
 }
