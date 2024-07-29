f90fc4263f85567b5a4bd0cf7526e332692fc49c
prog-daiki <t.daiki0913@icloud.com>
2024-07-29 20:53:32 +0900

    チャプターアクセス編集API

--- src/chapters/index.ts
+++ src/chapters/index.ts
@@ -312,4 +312,62 @@ Chapter.put(
   }
 );
 
+/**
+ * チャプターアクセス編集API
+ */
+Chapter.put(
+  "/:chapter_id/access",
+  zValidator(
+    "param",
+    z.object({
+      chapter_id: z.string(),
+      course_id: z.string(),
+    })
+  ),
+  zValidator(
+    "json",
+    insertChapterSchema.pick({
+      freeFlag: true,
+    })
+  ),
+  async (c) => {
+    // 認証チェック
+    const auth = getAuth(c);
+    if (!auth?.userId) {
+      return c.json({ error: Messages.MSG_ERR_001 }, 401);
+    }
+    const isAdmin = auth.userId === c.env.ADMIN_USER_ID;
+    if (!isAdmin) {
+      return c.json({ error: Messages.MSG_ERR_002 }, 401);
+    }
+
+    // パスパラメータを取得
+    const { course_id: courseId, chapter_id: chapterId } = c.req.valid("param");
+
+    // バリデーションチェック
+    const validatedData = c.req.valid("json");
+
+    // データベース接続
+    const db = getDbConnection(c.env.DATABASE_URL);
+    const courseLogic = new CourseLogic(db);
+    const chapterLogic = new ChapterLogic(db);
+
+    // 講座の存在チェック
+    const existsCourse = await courseLogic.checkCourseExists(courseId);
+    if (!existsCourse) {
+      return c.json({ error: Messages.MSG_ERR_003(Entity.COURSE) }, 404);
+    }
+
+    // チャプターの存在チェック
+    const existsChapter = await chapterLogic.checkChapterExists(chapterId);
+    if (!existsChapter) {
+      return c.json({ error: Messages.MSG_ERR_003(Entity.CHAPTER) }, 404);
+    }
+
+    const chapter = await chapterLogic.updateChapter(chapterId, validatedData);
+
+    return c.json(chapter);
+  }
+);
+
 export default Chapter;
