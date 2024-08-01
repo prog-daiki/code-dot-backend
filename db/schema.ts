440180378e88bee1e3667e892fdbdbe5a6f13950
prog-daiki <t.daiki0913@icloud.com>
2024-08-01 22:23:32 +0900

    スキーマの修正

--- db/schema.ts
+++ db/schema.ts
@@ -105,7 +105,7 @@ export const insertCourseSchema = createInsertSchema(course).extend({
   description: z
     .string()
     .min(1, "詳細は1文字以上です")
-    .max(100, "詳細は100文字以内です")
+    .max(1000, "詳細は1000文字以内です")
     .regex(/^[\p{L}\p{N}\s\-_.,]+$/u, "詳細に無効な文字が含まれています"),
   imageUrl: z
     .string()
