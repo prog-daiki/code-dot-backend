fd938da25934fe359809ceecdd201369326f2cb6
prog-daiki <t.daiki0913@icloud.com>
2024-09-08 11:47:53 +0900

    講座サムネイル編集APIのリファクタリング

--- src/core/courses/index.ts
+++ src/core/courses/index.ts
@@ -192,11 +192,10 @@ Course.put(
   zValidator("json", insertCourseSchema.pick({ imageUrl: true })),
   zValidator("param", z.object({ course_id: z.string() })),
   async (c) => {
+    const validatedData = c.req.valid("json");
+    const { course_id: courseId } = c.req.valid("param");
+    const courseUseCase = c.get("courseUseCase");
     try {
-      const validatedData = c.req.valid("json");
-      const { course_id: courseId } = c.req.valid("param");
-      const db = getDbConnection(c.env.DATABASE_URL);
-      const courseUseCase = new CourseUseCase(db);
       const course = await courseUseCase.updateCourseThumbnail(courseId, validatedData.imageUrl);
       return c.json(course);
     } catch (error) {
