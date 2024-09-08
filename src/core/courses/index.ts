5580cff1f7ba1ea9a021ca18076b638e0045028f
prog-daiki <t.daiki0913@icloud.com>
2024-09-08 11:17:11 +0900

    講座詳細編集APIのリファクタリング

--- src/core/courses/index.ts
+++ src/core/courses/index.ts
@@ -165,11 +165,10 @@ Course.put(
   zValidator("json", insertCourseSchema.pick({ description: true })),
   zValidator("param", z.object({ course_id: z.string() })),
   async (c) => {
+    const validatedData = c.req.valid("json");
+    const { course_id: courseId } = c.req.valid("param");
+    const courseUseCase = c.get("courseUseCase");
     try {
-      const validatedData = c.req.valid("json");
-      const { course_id: courseId } = c.req.valid("param");
-      const db = getDbConnection(c.env.DATABASE_URL);
-      const courseUseCase = new CourseUseCase(db);
       const course = await courseUseCase.updateCourseDescription(
         courseId,
         validatedData.description,
